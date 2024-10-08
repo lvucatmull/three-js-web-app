cf67e5bf4a8feb935dab68fce470d366a7f736d8
luvcatmul <devseongjoo2@gmail.com>
2024-10-08 17:15:29 +0900

    [bugfix] fix label for progress error : for => htmlFor

--- src/component/Loading/index.jsx
+++ src/component/Loading/index.jsx
@@ -3,7 +3,7 @@ import Typography from 'component/Typography';
 const Loading = ({ rate }) => {
   return (
     <div>
-      <label for="scene">
+      <label htmlFor="scene">
         <Typography p2>Loading Scene ...</Typography>
       </label>
       <progress id="scene" max="100" value={rate} />
