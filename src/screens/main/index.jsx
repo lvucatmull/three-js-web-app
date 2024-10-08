62fa64966ea78ab27761b1f5ecd44477367170d6
luvcatmul <devseongjoo2@gmail.com>
2024-10-08 17:12:37 +0900

    [feat] main 페이지 & button style

--- src/screens/main/index.jsx
+++ src/screens/main/index.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import View from './view';
 
+//Main Page
 const MainPage = () => {
   const [selected, setSelected] = useState(null);
+
+  console.log("/main page");
 
   const handleSelected = selection => {
     setSelected(selection);
   };
 
-  return <View selected={selected} handleSelected={handleSelected} />;
+  let answer = 0;
+  
+  function findseven(nine, ) {
+    
+  }
+  
+  return <View selected={selected} handleSelected={handleSelected} answer={answer} />;
 };
 
 export default MainPage;
--- src/screens/main/view.jsx
+++ src/screens/main/view.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import styles from 'style/style.module.scss';
 import NavHeader from 'component/nav';
+import styles from 'style/style.module.scss';
 
-const View = ({ selected, handleSelected }) => {
+//Main Page
+const View = ({ selected, handleSelected, answer }) => {
   console.log('[RENDER] MainPage');
 
   return (
     <div className={styles.container}>
       <NavHeader selected={selected} handleSelected={handleSelected} />
+      <div className={styles.body}>
+        <div className={styles.contents}>
+          <div className={styles.contentsBlock}>
+            <span className={styles.btnSelection}>
+              <p style={{fontSize: 15}}> Answer is ${answer} </p>
+            </span>
+            <span className={styles.btnSelection}>
+              <p style={{fontSize: 15}}> Answer is ${answer} </p>
+            </span>
+            <span className={styles.btnSelection}>
+              <p style={{fontSize: 15}}> Answer is ${answer} </p>
+            </span>
+            <span className={styles.btnSelection}>
+              <p style={{fontSize: 15}}> Answer is ${answer} </p>
+            </span>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
