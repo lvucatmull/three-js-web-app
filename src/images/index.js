7e1774466edc91dfa70e1f578f60e11f11d42879
luvcatmul <devseongjoo2@gmail.com>
2024-11-20 09:47:36 +0900

    🍱 add nav icon

--- src/images/index.js
+++ src/images/index.js
@@ -3,3 +3,5 @@ export { default as MainLogo } from './3d-logo.webp';
 export { default as MainLogoVector } from './3d-vector-logo.png';
 export { default as NameLogo } from './ksj-logo.webp';
 export { default as NameLogoVector } from './name-logo.png';
+export { default as Rocket } from './rocket-3d-icon.png';
+
