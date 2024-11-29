36b82943e1163965f27c75e466826435fb4978c1
luvcatmul <devseongjoo2@gmail.com>
2024-11-29 11:48:07 +0000

    🌱 add intersection

---
+++ src/hooks/intersection.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useIntersectionObserver(options) {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      setIsVisible(entry.isIntersecting);
+    }, options);
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, [options]);
+
+  return [elementRef, isVisible];
+}
+
+export default useIntersectionObserver;
