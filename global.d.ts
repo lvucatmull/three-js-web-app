e334b36e2db353d36e8f86e785d6407cced2b922
luvcatmul <devseongjoo2@gmail.com>
2024-12-24 08:50:56 +0000

    🧱 changes in settings related to typescript + infra

---
+++ global.d.ts
@@ -0,0 +1,24 @@
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const content: { [key: string]: string };
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
